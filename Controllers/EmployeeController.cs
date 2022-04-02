542642a5db381a043eb03521c99a2a09194678b6
rohibus <rohitsince1998@gmail.com>
2022-04-02 12:57:55 +0530

    FirstCommit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoWebApplication.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.ToList();
+            }
+        }
+        public Employee Get(int id)
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
