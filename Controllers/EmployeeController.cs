8b42b3353e5536265a6768043d4f7d9aebbd2f4b
rohibus <rohitsince1998@gmail.com>
2022-04-03 10:10:29 +0530

    CRUD Operation

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -9,18 +9,104 @@ namespace DemoWebApplication.Controllers
 {
     public class EmployeeController : ApiController
     {
-        public IEnumerable<Employee> Get()
+        public HttpResponseMessage Get()
         {
             using (EmployeeDBContext dbContext = new EmployeeDBContext())
             {
-                return dbContext.Employees.ToList();
+                var Employees = dbContext.Employees.ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, Employees);
             }
         }
-        public Employee Get(int id)
+
+        public HttpResponseMessage Get(int id)
         {
             using (EmployeeDBContext dbContext = new EmployeeDBContext())
             {
-                return dbContext.Employees.FirstOrDefault(e => e.ID == id);
+                var entity = dbContext.Employees.FirstOrDefault(e => e.ID == id);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                        "Employee with ID " + id.ToString() + "not found");
+                }
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBContext dbContext = new EmployeeDBContext())
+                {
+                    dbContext.Employees.Add(employee);
+                    dbContext.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri +
+                        employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBContext dbContext = new EmployeeDBContext())
+                {
+                    var entity = dbContext.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                            "Employee with Id " + id.ToString() + " not found to update");
+                    }
+                    else
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.Gender = employee.Gender;
+                        entity.Salary = employee.Salary;
+                        dbContext.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBContext dbContext = new EmployeeDBContext())
+                {
+                    var entity = dbContext.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                            "Employee with Id = " + id.ToString() + " not found to delete");
+                    }
+                    else
+                    {
+                        dbContext.Employees.Remove(entity);
+                        dbContext.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
     }
